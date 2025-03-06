55a17bf721e5763cff650550899f832c62a74ac5
MANRAL Rohan OBS/GDO <shnq0093@orange.com>
2025-03-06 12:31:37 +0530

    manral

--- rohan.sh
+++ rohan.sh
@@ -5,11 +5,8 @@ Believe | Become
 This is a new feature <Button of main>
 This is a new feature 
 <br>
-<<<<<<< HEAD
 Testing branch feature again. Welcome to Orange! Learnign from core!
-=======
-Testing branch feature again. Welcome to Orange! GITHUB. 
->>>>>>> main
+Testing branch feature again. Welcome to Orange! Manral. 
 Testing branch feature again. Working from office today.
 
 Wednesday evening!
